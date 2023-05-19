e23937970dbdc532d95d8bb2ffd2bc3b38267e69
Ricardo Barbosa <89495784+ricardobarbosadev@users.noreply.github.com>
2023-05-19 16:40:44 -0300

    secçao contacts. Ajuste de responsividade

---
+++ script.js
@@ -0,0 +1,12 @@
+const playlistId = '5uNDnqgerABvFlh5ztC6A4';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/5uNDnqgerABvFlh5ztC6A4?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
